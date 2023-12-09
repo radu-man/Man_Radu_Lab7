15c5951c468e8c081e6f2b5a8bf936c4d83344fa
Radu Man <manradu1986@yahoo.com>
2023-12-09 11:24:49 +0200

    Laborator8 bootstrap

--- Man_Radu_Lab7/App.xaml.cs
+++ Man_Radu_Lab7/App.xaml.cs
@@ -1,8 +1,27 @@
-﻿namespace Man_Radu_Lab7;
+﻿using System;
+using System.IO;
+using Man_Radu_Lab7.Data;
+
+namespace Man_Radu_Lab7;
 
 public partial class App : Application
 {
-	public App()
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
+    public App()
 	{
 		InitializeComponent();
 
